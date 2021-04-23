f58166f63ffeaef034a2c5f184c8ea3fadc02c2c
Luiz Gonzaga <lfilho@gmail.com>
2021-04-22 20:52:03 -0700

    feat: add google maps link

--- scripts/paragon_real_state_permalink.user.js
+++ scripts/paragon_real_state_permalink.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Paragon real state permalink
-// @version      0.1
+// @version      0.2
 // @description  Adds a permalink to paragon's listings so we can share with realtors, spouses, etc
 // @author       Luiz Filho
 // @match        https://bcres.paragonrels.com/publink/*
@@ -11,23 +11,53 @@
 // @license      MIT
 // ==/UserScript==
 
-(() => {
-  const rows = [
-    ...window.frames['left'].document.querySelectorAll(
-      'tr.GridAltRow, tr.GridRow, tr.GridHiRow'
-    ),
-  ];
-  const guid = new URL(document.location).searchParams.get('GUID');
+(async () => {
+  await sleep(2000);
 
-  rows.forEach(row => {
-    try {
-      const subject = JSON.parse(decodeURIComponent(row.dataset.subject));
-      const listingKey = subject.listingKey;
-      const externalLink = `https://bcres.paragonrels.com/publink/Report.aspx?outputtype=HTML&GUID=${guid}&ListingID=${listingKey}:0&Report=Yes&view=29&layout_id=65&screenWidth=1318`;
-      const newColumn = `<td><a href=${externalLink} target="_blank">⚓︎</a></td>`;
-      row.insertAdjacentHTML('afterbegin', newColumn);
-    } catch {
-      console.error('Row subject seems invalid');
-    }
-  });
+  addPermalinks();
+
+  addGoogleMapsLinks();
+
+  function addGoogleMapsLinks() {
+    const addressDiv = window.frames.fraDetail.document.elementFromPoint(
+      150,
+      15
+    );
+    const postalCodeDiv = window.frames.fraDetail.document.elementFromPoint(
+      316,
+      56
+    );
+    const address = [addressDiv, postalCodeDiv]
+      .map(el => el.innerText)
+      .join(', ');
+
+    const googleMapsLink = `<a href="https://www.google.com/maps/search/${address}" target="_blank">📍</a>`;
+
+    addressDiv.insertAdjacentHTML('beforeend', googleMapsLink);
+  }
+
+  function addPermalinks() {
+    const rows = [
+      ...window.frames.left.document.querySelectorAll(
+        'tr.GridAltRow, tr.GridRow, tr.GridHiRow'
+      ),
+    ];
+    const guid = new URL(document.location).searchParams.get('GUID');
+
+    rows.forEach(row => {
+      try {
+        const subject = JSON.parse(decodeURIComponent(row.dataset.subject));
+        const listingKey = subject.listingKey;
+        const externalUrl = `https://bcres.paragonrels.com/publink/Report.aspx?outputtype=HTML&GUID=${guid}&ListingID=${listingKey}:0&Report=Yes&view=29&layout_id=65&screenWidth=1318`;
+        const externalLink = `<a href=${externalUrl} target="_blank">🏠</a>&nbsp;`;
+        row.querySelector('td').insertAdjacentHTML('afterbegin', externalLink);
+      } catch {
+        console.error('Row subject seems invalid');
+      }
+    });
+  }
+
+  async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 })();
